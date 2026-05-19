4537cbb977026496398463b6e0a5b81ee33c1501
Bhanupratap0906 <bshekhawat645@medianv.com>
2026-05-19 08:50:55 +0530

    smpt issue reolved

--- api/index.js
+++ api/index.js
@@ -99,7 +99,9 @@ app.post("/send-mail", async (req, res) => {
     ========================= */
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,
